9156071d14907447bcb35a65d1bd82f862069cb0
«Vasiliy» <pyatin_vs@mail.ru>
2022-06-27 01:00:56 +0300

    Решена третья задача

---
+++ task003/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write ("Введите число: ");
+int num = int.Parse(ReadLine());
+int count = 1;
+
+// while (count<=num)
+// {
+//     Write ($"{count*count*count} ");
+//     count++;
+// }
+
+while (count<num)
+{
+    Write ($"{Math.Pow(count, 3)}, ");
+    count++;
+}
+Write ($"{Math.Pow(count, 3)}");
