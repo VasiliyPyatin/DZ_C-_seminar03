b5d7fce5b510c5e3d0c00bb6f82ead8cb674b19c
«Vasiliy» <pyatin_vs@mail.ru>
2022-06-27 00:27:10 +0300

    Решена вторая задача

---
+++ task002/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write ("Введите X1: ");
+int x1 = int.Parse(ReadLine());
+Write ("Введите Y1: ");
+int y1 = int.Parse(ReadLine());
+Write ("Введите Z1: ");
+int z1 = int.Parse(ReadLine());
+
+Write ("Введите X2: ");
+int x2 = int.Parse(ReadLine());
+Write ("Введите Y2: ");
+int y2 = int.Parse(ReadLine());
+Write ("Введите Z2: ");
+int z2 = int.Parse(ReadLine());
+
+// int X = x2-x1;
+// int Y = y2-y1;
+// int Z = z2-z1;
+
+double length = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+
+WriteLine($"Расстояние равно {length:f2}");
